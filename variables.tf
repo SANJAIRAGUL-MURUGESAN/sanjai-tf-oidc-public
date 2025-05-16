b6ad371a28598bcf637c7bbe475e60636cc455eb
Sanjai Ragul Murugesh <sanjairagulm@gmail.com>
2025-05-16 12:32:46 +0530

    Initial Commit - Main Branch

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default = "sanjai-oidc"
+}
+
+variable "bucket_acl" {
+  description = "The ACL for the S3 bucket"
+  type        = string
+  default     = "private"
+}
+
+// ADD
+
