d46ca65dc1acb7310a07361e3390ffad8ee18871
Sanjai Ragul Murugesh <sanjairagulm@gmail.com>
2025-05-26 19:36:36 +0530

    Updated ACM v1

--- modules/acm_certificate/main.tf
+++ modules/acm_certificate/main.tf
@@ -1,12 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_acm_certificate" "this" {
   domain_name               = var.primary_domain
   subject_alternative_names = var.alternative_domains
   validation_method         = "DNS"
-  provider                  = aws.us_east_1
+  # provider                  = var.aws
 
   lifecycle {
     create_before_destroy = true
   }
 
-  tags = var.tags
+  tags = var.acm_tags
 }
--- modules/acm_certificate/variables.tf
+++ modules/acm_certificate/variables.tf
@@ -13,3 +13,9 @@ variable "acm_tags" {
   type        = map(string)
   description = "Tags to apply to s3 Bucket"
 }
+
+variable "aws" {
+  type = string
+  description = "Provider for AWS Certificate Manager to issue a certificate"
+  default = "us-east-1"
+}
