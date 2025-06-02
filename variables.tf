11775cef54ad709afb95b86c58dfa13680e2cc51
Sanjai Ragul Murugesh <sanjairagulm@gmail.com>
2025-06-02 16:00:21 +0530

    Updated Role for WAF Policy Issue v9

--- variables.tf
+++ variables.tf
@@ -120,22 +120,22 @@ variable "cloudfront_tags" {
 
 # variables for log group
 
-variable "cloudwatch_loggroup_name" {
-  description = "Name of the CloudWatch Log Group"
-  type        = string
-}
+# variable "cloudwatch_loggroup_name" {
+#   description = "Name of the CloudWatch Log Group"
+#   type        = string
+# }
 
-variable "cloudwatch_loggroup_retention_in_days" {
-  description = "Retention period for logs"
-  type        = number
-  default     = 30
-}
+# variable "cloudwatch_loggroup_retention_in_days" {
+#   description = "Retention period for logs"
+#   type        = number
+#   default     = 30
+# }
 
-variable "cloudwatch_tags" {
-  description = "Tags for the log group"
-  type        = map(string)
-  default     = {}
-}
+# variable "cloudwatch_tags" {
+#   description = "Tags for the log group"
+#   type        = map(string)
+#   default     = {}
+# }
 
 
 # variables for WAF
